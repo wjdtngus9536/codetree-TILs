a3cb4748ba750ba82d5c8b245b1410202041bc8c
soohyun <108653304+wjdtngus9536@users.noreply.github.com>
2023-11-23 22:23:29 +0900

    Title: 정수 선언하고 뺄셈, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231123/정수 선언하고 뺄셈/define-numbers-and-substract.py
@@ -0,0 +1,6 @@
+# 변수나 함수명은 소문자로
+# 여러 단어를 사용하는 경우 _(언더바) 사용
+
+integer_variable1, integer_variable2 = 97, 13
+
+print(integer_variable1 ,'-', integer_variable2, '=', integer_variable1 - integer_variable2)
